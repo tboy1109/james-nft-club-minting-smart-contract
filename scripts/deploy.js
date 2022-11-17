882e9bba0f17e33f3004c5ca0e45590f5d784a37
tboy1109 <tboy1109@hotmail.com>
2022-11-17 15:32:05 -0600

    Upload Project

---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const James = await hre.ethers.getContractFactory("JamesNFTClub");
+  const james = await James.deploy("Hello, Hardhat!");
+
+  await james.deployed();
+
+  console.log("JamesNFTClub deployed to:", james.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
